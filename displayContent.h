00e41ae8de72724bf8ebf9977b61ae5f2899afa4
iamwatchdogs <shamith301102@gmail.com>
2023-04-25 01:13:57 +0530

    Finished documentation

--- displayContent.h
+++ displayContent.h
@@ -83,16 +83,34 @@ void displayCommandLineArgs ( void )
 	puts("-------------------------------------------------------------------------------------------\n");
 	
 	puts("For the current version, the following options (or) Arguments are supported:\n");
-	puts("-a  (or) --args      : To display the all available argumentsof this version of the program.\n");
-	puts("-c  (or) --commands  : To view to commands being executed to fix the DNS problem.\n");
-	puts("-dr (or) --directrun : Directly runs the DNS fixing commands without Admin check. (Not Recommended for end-users)");
-	puts("-h  (or) --help      : To read the following content of this program.\n");
-	puts("-p  (or) --project   : Redirects to the Original Open-Source Project.\n");
-	puts("-r  (or) --run       : Runs the commands to fix the DNS issue.\n");
-	puts("-u  (or) --update    : Redirects to the releases of this Project.\n");
-	puts("-v  (or) --version   : To know the current version.\n");
+	puts("-a  (or) --args       : To display the all available argumentsof this version of the program.\n");
+	puts("-c  (or) --commands   : To view to commands being executed to fix the DNS problem.\n");
+	puts("-dr (or) --directrun  : Directly runs the DNS fixing commands without Admin check. (Not Recommended for end-users)");
+	puts("-h  (or) --help       : To read the following content of this program.\n");
+	puts("-p  (or) --project    : Redirects to the Original Open-Source Project.\n");
+	puts("-pn (or) --patchnotes : Runs the commands to fix the DNS issue.\n");
+	puts("-r  (or) --run        : Runs the commands to fix the DNS issue.\n");
+	puts("-u  (or) --update     : Redirects to the releases of this Project.\n");
+	puts("-v  (or) --version    : To know the current version.\n");
 	
 	puts("\n-------------------------------------------------------------------------------------------");
+	return ;
+}
+
+void displayPatchNotes ( void )
+{
+    puts("\n-------------------------------------------");
+	printf("\nPatch Note (New Release %s):\n",VERSION);
+    puts("-------------------------------------------\n");
+	puts("* Completely reworked on the pervious version of orginal source code for this project.");
+	puts("* Updated commandline arguments for better interaction.");
+	puts("* Improved end-user interaction through Interactive and CommandLine sessions.");
+	puts("* Implemented the auto-check for admin access for executing DNS fixing commands.");
+	puts("* Implemented auto-relaunch to execute the commands after seeking end-user\'s permission.");
+	puts("* Implemented Error-prompting for debugging this utility program.");
+	puts("* Added Source to redirect the program to the original project.");
+	puts("* Added Source to redirect the program to check for latest releases.");
+	puts("* Improved Documentation.");
 	return ;
 }
 
@@ -109,6 +127,7 @@ void helpDocumentation ( void )
 	puts("Created on: 26th September 2022");
 	puts("Updated on: 18th April 2023");
 	puts("Required Access: Administrator");
+	puts("Operating System: Windows 10 or above");
 	
 	puts("\n-------------------------------------------");
 	puts("Description:");
@@ -117,22 +136,24 @@ void helpDocumentation ( void )
 	printf("\"dns_fix.exe\" is a program is used to fix the \"DNS not responding problem.\" in WINDOWS OS.");
 	printf("These are set of command line commands which deals with this problem even after setting their DNS server manually to the ones of the google standard one.");
 	puts("If you haven\'t tried the first option, then I suggest you to try fix it that way because most of the times it fixes just by manually setting the DNS server address.");
+	puts("\nTry to find these steps in my original project, you can directly redirect to the project using \'-p\' or \'--project\' arguments.\n");
 	
-	puts("\nFor more details, You can checkout this project website: https://github.com/iamwatchdogs/Windows_DNS_Fixer");
-	
-	puts("\nNote:");
-	printf("Make sure that this program is execute under Administrator level access, as some the commands used in this program require Administrator level access.");
-	puts("So, to make sure that this program fulfill it purpose successfully. Run the cmd ( if you are will to execute it on cmd ) or the dns_fix.exe file as Administrator");
-
-    insideDnsFix();
+	insideDnsFix();
     
 	displayCommandLineArgs();
 
-    puts("\n-------------------------------------------");
-	printf("\nPatch Note (New Release %s):\n",VERSION);
-    puts("-------------------------------------------\n");
-	// puts("\tWill fill at the end of the renewly");
+	puts("\nNote:");
+	puts("- Please try to use this untility program if and only if you're facing DNS issue even after following basic troubleshooting steps.");
+	puts("- Some of the commands require Adminstrator Access for executing the DNS issue resolving commands.");
+	puts("- Please try to avoid using \"-dr\" or \"--directrun\" arguments, as they execute the commands without checking for Admin access.");
+	puts("- Never use \"-dr\" or \"--directrun\" arguments, if you don't know what you\'re doing.");
+	puts("- The direct run option is purposefully not included in Interactive Session for security reasons.");
+	puts("- If you're a begineer who want to learn & understand, then you can checkout orginal project.");
+
+    displayPatchNotes();
 	
+	puts("\nFor more details, You can checkout this project website: https://github.com/iamwatchdogs/Windows_DNS_Fixer");
+
 	return ;
 }
 
